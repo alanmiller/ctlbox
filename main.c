ae9765124e7f3bf9a97c92869397fda4e4675553
Alan Miller <alan.miller3@gmail.com>
2023-10-22 20:05:48 -0400

    buttons working

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include "pico/cyw43_arch.h"
 #include <inttypes.h>
 #include "tusb.h"
+#include "tusb_config.h"
 #include "usb_descriptors.h"
-#include "class/hid/hid_device.h"
 
 //    button number       1      2     3    4      5     6     7     8      9    10
 //    keycode             i      o    F1    k     F3    F9     9     0    left  right
@@ -12,6 +12,7 @@
 uint8_t key_usage[10] = {0x0c, 0x12, 0x3a, 0x0e, 0x3c, 0x42, 0x26, 0x27, 0x50, 0x4f};
 uint button_pins[10] =  {6, 7, 8, 9, 10, 11, 12, 13, 20, 21};
 bool button_state[10] = {false};
+
 
 // This callback will be invoked when we need to send a report.
 void tud_hid_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t bufsize) {
@@ -28,51 +29,64 @@ void tud_hid_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t
 }
 
 bool read_button_state(int button) {
-    return gpio_get(button_pins[button]);
+    // Assuming button_pins[i] is the GPIO pin number for button i
+    // and the button is wired with a pull-up resistor
+    // gpio_get() should return false when the button is pressed and true when it's not pressed
+    return !gpio_get(button_pins[button]);
 }
 
 int main(void) {
-  // puts("boot");
-
   // Initialize the Pico.
   stdio_init_all();
   tusb_init();
   
+  puts("Initializing controlbox firmware....");
   if (cyw43_arch_init()) {
-    printf("Wi-Fi init failed");
+    puts("  Wi-Fi init failed");
+  } else {
+    puts("  Wi-Fi initialized.");
+    // cyw43_arch_enable_sta_mode();
   }
 
-  puts("Initializing controlbox firmware....");
-  
   // Initialize the GPIO pins
   for (int i = 0; i < 10; i++) {
     gpio_init(button_pins[i]);
+    gpio_pull_up(button_pins[i]);
     gpio_set_dir(button_pins[i], GPIO_IN);
   }
+  puts("  Buttons initialized.");
+  puts("  Polling buttons:");
 
   while (1) {
         // Let TinyUSB do its work
         tud_task();
-
         // Update the button states and send a report if necessary
+        uint8_t keycode[6] = { 0, 0, 0, 0, 0, 0 };
         for (int i = 0; i < 10; i++) {
             bool new_state = read_button_state(i);
-            //bool new_state = gpio_get(button_pins[i]);
-            if (button_state[i] != new_state) {
-                button_state[i] = new_state;
-                // either send a keyboard(1)  or a gamepad report(2)
+            bool old_state = button_state[i];
+            //printf("Debug: Button %d, old_state: %s, new_state: %s\n", i+1, old_state ? "true" : "false", new_state ? "true" : "false");
+            if (old_state != new_state) {
+                // Button state has changed; send a keyboard(1)  or a gamepad report(2)
                 if (new_state) {
-                  // Key press
-                  uint8_t keycode[10] = { key_usage[i], 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-                  tud_hid_keyboard_report(1, 0, keycode);
+                  // Key press set the first keycode to the key usag
+                  printf("  Button %d, gpio %d, old/new state: %s/%s , sending: %x\n",
+                        i+1, 
+                        (int)button_pins[i], 
+                        old_state ? "true" : "false", 
+                        new_state ? "true" : "false", 
+                        key_usage[i]);
+                  keycode[0] = key_usage[i];
                 } else {
                   // Key release
-                  uint8_t keycode[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-                  tud_hid_keyboard_report(1, 0, keycode);
+                  printf("  Button %d released\n", i+1 );
+                  memset(keycode, 0, sizeof(keycode));
                 }
+                tud_hid_keyboard_report(1, 0, keycode);
             }
+            // update states
+            button_state[i] = new_state; 
         }
-    }
-  
+  }
   return 0;
 }
--- usb_descriptors.h
+++ usb_descriptors.h
@@ -14,7 +14,6 @@ extern uint8_t const* tud_descriptor_device_cb(void);
 extern uint8_t const* tud_descriptor_configuration_cb(uint8_t index);
 extern uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid);
 extern uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen);
-//extern void tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t bufsize);
 extern void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize);
 extern bool hidd_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const * request);
 
