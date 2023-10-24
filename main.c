9bf3631f35dac17ee12c67509712269b8756d2b2
Alan Miller <alan.miller3@gmail.com>
2023-10-24 11:30:14 -0400

    fixed report descriptor

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ uint8_t key_usage[10] = {0x0c, 0x12, 0x3a, 0x0e, 0x3c, 0x42, 0x26, 0x27, 0x50, 0
 uint button_pins[10] =  {6, 7, 8, 9, 10, 11, 12, 13, 20, 21};
 bool button_state[10] = {false};
 
-
 // This callback will be invoked when we need to send a report.
 void tud_hid_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t bufsize) {
     // Clear the report
@@ -25,6 +24,26 @@ void tud_hid_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t
         if (button_state[i] && key_count < 10) {
             buffer[key_count++] = key_usage[i];
         }
+    }
+}
+
+void button_press_handler(uint button_id, bool pressed) {
+    // Map the button ID to a key usage code
+    // uint8_t key_usage = button_to_key_usage(button_id);
+    uint8_t key = key_usage[button_id];
+
+    if (pressed) {
+        // The button was pressed
+        // printf("  Button %d pressed, sending key: %c\n", button_id, key);
+
+        // Send a HID report to press the key
+        uint8_t keycode[6] = { key, 0, 0, 0, 0, 0 };
+        tud_hid_keyboard_report(1, 0, keycode);
+    } else {
+        // The button was released
+        // Send a HID report to release the key
+        uint8_t keycode[6] = { 0, 0, 0, 0, 0, 0 };
+        tud_hid_keyboard_report(1, 0, keycode);
     }
 }
 
@@ -41,8 +60,11 @@ int main(void) {
   tusb_init();
   
   puts("Initializing controlbox firmware....");
+  puts("  Getting serial number.");
+  get_serial_number();
+  
   if (cyw43_arch_init()) {
-    puts("  Wi-Fi init failed");
+    puts("  Wi-Fi init failed.");
   } else {
     puts("  Wi-Fi initialized.");
     // cyw43_arch_enable_sta_mode();
@@ -55,21 +77,22 @@ int main(void) {
     gpio_set_dir(button_pins[i], GPIO_IN);
   }
   puts("  Buttons initialized.");
-  puts("  Polling buttons:");
-
+  puts("  Ready for use.");
   while (1) {
         // Let TinyUSB do its work
         tud_task();
         // Update the button states and send a report if necessary
         uint8_t keycode[6] = { 0, 0, 0, 0, 0, 0 };
+        bool pressed = false;
         for (int i = 0; i < 10; i++) {
             bool new_state = read_button_state(i);
             bool old_state = button_state[i];
-            //printf("Debug: Button %d, old_state: %s, new_state: %s\n", i+1, old_state ? "true" : "false", new_state ? "true" : "false");
+
             if (old_state != new_state) {
-                // Button state has changed; send a keyboard(1)  or a gamepad report(2)
+                // sleep_ms(20); // Debouncing delay
+                // Button state has changed; send a keyboard(1) or a gamepad report(2)
                 if (new_state) {
-                  // Key press set the first keycode to the key usag
+                  // Key press set the first keycode to the key usage
                   printf("  Button %d, gpio %d, old/new state: %s/%s , sending: %x\n",
                         i+1, 
                         (int)button_pins[i], 
@@ -77,12 +100,14 @@ int main(void) {
                         new_state ? "true" : "false", 
                         key_usage[i]);
                   keycode[0] = key_usage[i];
+                  pressed = true;
                 } else {
                   // Key release
                   printf("  Button %d released\n", i+1 );
                   memset(keycode, 0, sizeof(keycode));
+                  pressed = false;
                 }
-                tud_hid_keyboard_report(1, 0, keycode);
+                button_press_handler(i,pressed);
             }
             // update states
             button_state[i] = new_state; 
--- usb_descriptors.c
+++ usb_descriptors.c
@@ -1,4 +1,26 @@
 #include "usb_descriptors.h"
+#include "hardware/regs/addressmap.h"
+#include "pico/unique_id.h"
+
+uint16_t string_desc[32];
+uint32_t serial_number[4];
+
+pico_unique_board_id_t board_id;
+char serial_number_str[25];
+
+void get_serial_number() {
+    pico_get_unique_board_id(&board_id);
+    for (int i = 0; i < PICO_UNIQUE_BOARD_ID_SIZE_BYTES; ++i) {
+        snprintf(serial_number_str + i * 2, 3, "%02x", board_id.id[i]);
+    }
+}
+
+char const *string_desc_arr [] = {
+    (const char[]) {0x09, 0x04}, // 0: is supported language is English (0x0409)
+    "AlanMiller",                // 1: Manufacturer
+    "GolfSimController",         // 2: Product
+    serial_number_str,           // 3: Serials, should use chip ID
+};
 
 tusb_desc_device_t const desc_device = {
     .bLength            = sizeof(tusb_desc_device_t),
@@ -13,12 +35,15 @@ tusb_desc_device_t const desc_device = {
     .idProduct          = 0x5678, // Product ID
     .bcdDevice          = 0x0100,
 
-    .iManufacturer      = 0x01, // Index of Manufacturer string descriptor
-    .iProduct           = 0x02, // Index of Product string descriptor
-    .iSerialNumber      = 0x03, // Index of Serial number string descriptor
+    .iManufacturer      = 1, // Index of Manufacturer string descriptor
+    .iProduct           = 2, // Index of Product string descriptor
+    .iSerialNumber      = 3, // Index of Serial number string descriptor
 
     .bNumConfigurations = 0x01
 };
+// URL for the firmware
+char const *url = "https://github.com/alanmiller/ctlbox";
+
 uint8_t const desc_configuration[] = {
     // Configuration descriptor
     0x09, // bLength
@@ -38,7 +63,7 @@ uint8_t const desc_configuration[] = {
     0x01, // bNumEndpoints 1
     0x03, // bInterfaceClass
     0x01, // bInterfaceSubClass
-    0x02, // bInterfaceProtocol
+    0x01, // bInterfaceProtocol keyboard 1, mouse 2
     0x00, // iInterface (String Index)
 
     // HID descriptor
@@ -48,7 +73,7 @@ uint8_t const desc_configuration[] = {
     0x00, // bCountryCode
     0x01, // bNumDescriptors
     0x22, // bDescriptorType[0] (HID)
-    0x34, 0x00, // wDescriptorLength[0] 52
+    0x43, 0x00, // wDescriptorLength[0] 67
 
     // Endpoint descriptor
     0x07, // bLength
@@ -64,9 +89,18 @@ uint8_t const desc_hid_report[] = {
 };
 
 // Callback functions
-uint8_t const* tud_hid_descriptor_report_cb(uint8_t instance) {
+
+// Invoked when received GET HID REPORT DESCRIPTOR request
+// Application return pointer to descriptor
+// Descriptor contents must exist long enough for transfer to complete
+uint8_t const * tud_hid_descriptor_report_cb(uint8_t instance) {
+    (void) instance;
     return desc_hid_report;
 }
+
+//uint8_t const* tud_hid_descriptor_report_cb(uint8_t instance) {
+//    return desc_hid_report;
+//}
 
 uint8_t const* tud_descriptor_device_cb(void) {
     return (uint8_t const*)&desc_device;
@@ -79,12 +113,43 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
 void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
     // Do nothing for now. Later we would control an led or something here.
 }
+
 uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
-    return NULL; // No string descriptors
+    
+    printf("tud_descriptor_string_cb called with index %d, langid %04x\n", index, langid);
+    printf("HID report descriptor length: %zu\n", sizeof(desc_hid_report));
+
+    (void) langid;
+    uint8_t chr_count = 0;
+
+    if (index == 0) {
+        memcpy(string_desc, string_desc_arr[0], 2);
+        chr_count = 1;
+        printf("Language ID: 0x%04x\n", string_desc[1]);
+    } else {
+        // Convert ASCII string into UTF-16
+        if (!(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0]))) return NULL;
+        const char* str = string_desc_arr[index];
+
+        // Cap at max char
+        chr_count = strlen(str);
+        if (chr_count > 31) chr_count = 31;
+
+        string_desc[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
+
+        for(uint8_t i=0; i<chr_count; i++) {
+            string_desc[1+i] = str[i];
+            }
+    }
+    // Print the string descriptor
+    printf("String descriptor: ");
+    for (uint8_t i = 0; i < chr_count; i++) {
+        printf("%c", string_desc[1+i]);
+    }
+    printf("\n");
+    return string_desc;
 }
+
 uint8_t const * tud_descriptor_configuration_cb(uint8_t index) {
     return (uint8_t const *)&desc_configuration;
 }
-//bool hidd_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const * request) {
-//    return false; // Request not handled
-//}
